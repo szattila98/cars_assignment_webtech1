57ab4fc4457d39d0588a1a0dd48d8bf999f00162
szattila98 <clownface68@gmail.com>
2018-10-30 20:06:11 +0100

    Added basic fade in-out JQuery for the soon-to be lister tables.

--- student/js/index.js
+++ student/js/index.js
@@ -7,4 +7,26 @@ $(document).ready(function () {
     $("#BtnBiz").click(function () {
         alert("We both know that's not gonna happen, fwend!");
     });
+
+    $("#banner").click(function () {
+        $("#description").fadeIn(900);
+        // $("#otherPart").show();
+        $(".contentImage").fadeIn(900);
+        $("#listManuf").fadeOut(1);
+        $("#listCar").fadeOut(1);
+    });
+
+    $("#menuTable td:contains('List Manufacturers')").click(function () {
+        $("#description").fadeOut(700);
+        $(".contentImage").fadeOut(700);
+        $("#listManuf").fadeIn(700);
+        $("#listCar").fadeOut(0);
+    });
+
+    $("#menuTable td:contains('List Cars')").click(function () {
+        $("#description").fadeOut(700);
+        $(".contentImage").fadeOut(700);
+        $("#listCar").fadeIn(700);
+        $("#listManuf").fadeOut(0);
+    });
 });
