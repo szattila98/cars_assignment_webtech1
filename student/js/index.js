65f22cd71d39ca2c141d80d5daa75a25aacfe5ed
szattila98 <clownface68@gmail.com>
2018-11-08 13:26:31 +0100

    Started to rework it AGAINNN.....

--- student/js/index.js
+++ student/js/index.js
@@ -18,74 +18,16 @@ $(document).ready(function () {
         $("#cookie").fadeOut(0);
     });
 
-    $("#menuTable td:contains('List Manufacturers')").click(function () {
-        $("#description").fadeOut(700);
-        $(".contentImage").fadeOut(700);
-        $("#listManuf").fadeIn(700);
-        $("#listCar").fadeOut(0);
-        $("#addCar").fadeOut(0);
-        $("#addManuf").fadeOut(0);
-        $("#cookie").fadeOut(0);
-
-        $.getJSON('manufacturers', function (data) {
-            var table = $('<table id="listTableManuf"></table>');
-            table.append('<tr><th class="listth">Name</th><th class="listth">Country</th><th class="listth">Founded</th></tr>');
-            $.each(data, function (key, value) {
-                var row = $('<tr></tr>');
-                var nameCell = $('<td class="listtd">' + value.name + '</td>');
-                var countryCell = $('<td class="listtd">' + value.country + '</td>');
-                var foundedCell =  $('<td class="listtd">' + value.founded + '</td>');
-                row.append(nameCell);
-                row.append(countryCell);
-                row.append(foundedCell);
-                table.append(row)
-            });
-            $('#listManuf').html(table);
-        });
-    });
-
-    $("#menuTable td:contains('List Cars')").click(function () {
-        $("#description").fadeOut(700);
-        $(".contentImage").fadeOut(700);
-        $("#listCar").fadeIn(700);
-        $("#listManuf").fadeOut(0);
-        $("#addCar").fadeOut(0);
-        $("#addManuf").fadeOut(0);
-        $("#cookie").fadeOut(0);
-
-        $.getJSON('cars', function (data) {
-            var table = $('<table id="listTableCar"></table>');
-            table.append('<tr><th class="listth">Name</th><th class="listth">Consumption</th><th class="listth">Color</th><th class="listth">Manufacturer</th><th class="listth">Available</th><th class="listth">Year</th><th class="listth">Horsepower</th></tr>');
-            $.each(data, function (key, value) {
-                var row = $('<tr></tr>');
-                var nameCell = $('<td class="listtd">' + value.name + '</td>');
-                var consumptionCell = $('<td class="listtd">' + value.consumption + '</td>');
-                var colorCell =  $('<td class="listtd">' + value.color + '</td>');
-                var manufacturerCell =  $('<td class="listtd">' + value.manufacturer.toString() + '</td>');
-                var availableCell =  $('<td class="listtd">' + value.available + '</td>');
-                var yearCell =  $('<td class="listtd">' + value.year + '</td>');
-                var horsepowerCell =  $('<td class="listtd">' + value.horsepower + '</td>');
-                row.append(nameCell);
-                row.append(consumptionCell);
-                row.append(colorCell);
-                row.append(manufacturerCell);
-                row.append(availableCell);
-                row.append(yearCell);
-                row.append(horsepowerCell);
-                table.append(row)
-            });
-            $('#listCar').html(table);
-        });
-    });
 
     $.get('manufacturerNames', function (data) {
         var count = 0;
         var $dropdown = $("#dropdown");
-        $.each(data, function() {
+        $.each(data, function () {
             $dropdown.append($("<option />").val(data[count]).text(data[count]));
             count = count + 1;
         });
     });
+
 
     $("#menuTable td:contains('Add Car')").click(function () {
         $("#description").fadeOut(700);
@@ -95,7 +37,7 @@ $(document).ready(function () {
         $("#addCar").fadeIn(700);
         $("#addManuf").fadeOut(0);
         $("#cookie").fadeOut(0);
-        });
+    });
 
     $("#menuTable td:contains('Add Manufacturer')").click(function () {
         $("#description").fadeOut(700);
@@ -107,40 +49,132 @@ $(document).ready(function () {
         $("#cookie").fadeOut(0);
     });
 
-    $("#menuTable td:contains('Cookie is walking!')").click(function () {
-        $("#description").fadeOut(700);
-        $(".contentImage").fadeOut(700);
-        $("#listManuf").fadeOut(0);
-        $("#listCar").fadeOut(0);
-        $("#addCar").fadeOut(0);
-        $("#addManuf").fadeOut(0);
-        $("#cookie").fadeIn(700);
+    $('form').on("submit", function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'post',
+            url: 'addCar',
+            data: $('form').serialize(),
+            success: function () {
+                listCars();
+            },
+            error: function () {
+                alert("oops");
+            }
+        })
+    });
 
-        document.cookie = 'name='+"Opel";
-
-        $.getJSON('manufacturer', function (data) {
-            var table = $('<table id="listTableCar"></table>');
-            table.append('<tr><th class="listth">Name</th><th class="listth">Consumption</th><th class="listth">Color</th><th class="listth">Manufacturer</th><th class="listth">Available</th><th class="listth">Year</th><th class="listth">Horsepower</th></tr>');
-            $.each(data, function (key, value) {
-                var row = $('<tr></tr>');
-                var nameCell = $('<td class="listtd">' + value.name + '</td>');
-                var consumptionCell = $('<td class="listtd">' + value.consumption + '</td>');
-                var colorCell =  $('<td class="listtd">' + value.color + '</td>');
-                var manufacturerCell =  $('<td class="listtd">' + value.manufacturer.toString() + '</td>');
-                var availableCell =  $('<td class="listtd">' + value.available + '</td>');
-                var yearCell =  $('<td class="listtd">' + value.year + '</td>');
-                var horsepowerCell =  $('<td class="listtd">' + value.horsepower + '</td>');
-                row.append(nameCell);
-                row.append(consumptionCell);
-                row.append(colorCell);
-                row.append(manufacturerCell);
-                row.append(availableCell);
-                row.append(yearCell);
-                row.append(horsepowerCell);
-                table.append(row)
-            });
-            $('#cookie').html(table);
-        });
+    $('form').on("submit", function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'post',
+            url: 'addManufacturer',
+            data: $('form').serialize(),
+            success: function () {
+                listCars();
+            },
+            error: function () {
+                alert("oops");
+            }
+        })
     });
 
 });
+
+function listCars() {
+
+    $("#description").fadeOut(700);
+    $(".contentImage").fadeOut(700);
+    $("#listCar").fadeIn(700);
+    $("#listManuf").fadeOut(0);
+    $("#addCar").fadeOut(0);
+    $("#addManuf").fadeOut(0);
+    $("#cookie").fadeOut(0);
+
+    $.getJSON('cars', function (data) {
+        var table = $('<table id="listTableCar"></table>');
+        table.append('<tr><th class="listth">Name</th><th class="listth">Consumption</th><th class="listth">Color</th><th class="listth">Manufacturer</th><th class="listth">Available</th><th class="listth">Year</th><th class="listth">Horsepower</th></tr>');
+        $.each(data, function (key, value) {
+            var row = $('<tr></tr>');
+            var nameCell = $('<td class="listtd">' + value.name + '</td>');
+            var consumptionCell = $('<td class="listtd">' + value.consumption + '</td>');
+            var colorCell = $('<td class="listtd">' + value.color + '</td>');
+            var manufacturerCell = $('<td class="listtd">' + value.manufacturer.toString() + '</td>');
+            var availableCell = $('<td class="listtd">' + value.available + '</td>');
+            var yearCell = $('<td class="listtd">' + value.year + '</td>');
+            var horsepowerCell = $('<td class="listtd">' + value.horsepower + '</td>');
+            row.append(nameCell);
+            row.append(consumptionCell);
+            row.append(colorCell);
+            row.append(manufacturerCell);
+            row.append(availableCell);
+            row.append(yearCell);
+            row.append(horsepowerCell);
+            table.append(row)
+        });
+        $('#listCar').html(table);
+    });
+
+}
+
+function listManuf() {
+    $("#description").fadeOut(700);
+    $(".contentImage").fadeOut(700);
+    $("#listManuf").fadeIn(700);
+    $("#listCar").fadeOut(0);
+    $("#addCar").fadeOut(0);
+    $("#addManuf").fadeOut(0);
+    $("#cookie").fadeOut(0);
+
+    $.getJSON('manufacturers', function (data) {
+        var table = $('<table id="listTableManuf"></table>');
+        table.append('<tr><th class="listth">Name</th><th class="listth">Country</th><th class="listth">Founded</th></tr>');
+        $.each(data, function (key, value) {
+            var row = $('<tr></tr>');
+            var nameCell = $('<td class="listtd">' + value.name + '</td>');
+            var countryCell = $('<td class="listtd">' + value.country + '</td>');
+            var foundedCell = $('<td class="listtd">' + value.founded + '</td>');
+            row.append(nameCell);
+            row.append(countryCell);
+            row.append(foundedCell);
+            table.append(row)
+        });
+        $('#listManuf').html(table);
+    });
+}
+
+function cookieF() {
+    $("#description").fadeOut(700);
+    $(".contentImage").fadeOut(700);
+    $("#listManuf").fadeOut(0);
+    $("#listCar").fadeOut(0);
+    $("#addCar").fadeOut(0);
+    $("#addManuf").fadeOut(0);
+    $("#cookie").fadeIn(700);
+
+    document.cookie = 'name=' + "Opel";
+
+    $.getJSON('manufacturer', function (data) {
+        var table = $('<table id="listTableCar"></table>');
+        table.append('<tr><th class="listth">Name</th><th class="listth">Consumption</th><th class="listth">Color</th><th class="listth">Manufacturer</th><th class="listth">Available</th><th class="listth">Year</th><th class="listth">Horsepower</th></tr>');
+        $.each(data, function (key, value) {
+            var row = $('<tr></tr>');
+            var nameCell = $('<td class="listtd">' + value.name + '</td>');
+            var consumptionCell = $('<td class="listtd">' + value.consumption + '</td>');
+            var colorCell = $('<td class="listtd">' + value.color + '</td>');
+            var manufacturerCell = $('<td class="listtd">' + value.manufacturer.toString() + '</td>');
+            var availableCell = $('<td class="listtd">' + value.available + '</td>');
+            var yearCell = $('<td class="listtd">' + value.year + '</td>');
+            var horsepowerCell = $('<td class="listtd">' + value.horsepower + '</td>');
+            row.append(nameCell);
+            row.append(consumptionCell);
+            row.append(colorCell);
+            row.append(manufacturerCell);
+            row.append(availableCell);
+            row.append(yearCell);
+            row.append(horsepowerCell);
+            table.append(row)
+        });
+        $('#cookie').html(table);
+    });
+}
